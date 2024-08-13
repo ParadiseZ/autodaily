15573a01b26c77696106f85a420a1fdd11517d79
PA <zhuzj2019@qq.com>
2024-08-13 08:02:21 +0800

    MatchMethod.kt 1、考虑canny只转换一次 2、backup→ncnn

--- app/src/main/java/com/smart/autodaily/handler/MatchMethod.kt
+++ app/src/main/java/com/smart/autodaily/handler/MatchMethod.kt
@@ -10,11 +10,10 @@ import org.opencv.imgcodecs.Imgcodecs
 import org.opencv.imgproc.Imgproc
 import splitties.init.appCtx
 import java.io.InputStream
-import kotlin.math.roundToInt
 
 const val scaledOriginalWidth = 1280
 const val scaledOriginalHeight = 720
-
+val tempMap = HashMap<String, ArrayList<Mat>>()
 /*fun templateMatch(
     picPath : String,
     picNameList : List<String>?,
@@ -82,57 +81,68 @@ fun templateMatch(
     matchSave: Boolean
 ) : Boolean{
     val result = Mat()
-    picNameList?.forEach {
-        println("picPath:$picPath/$it.png")
-        val  templateMat= getPicture("$picPath/$it.png")
-        var currentSimilar = 1.1
-        val scaledOriginalMat = Mat()
-        var res = Core.MinMaxLocResult()
-        //println("rows: ${originalMat.rows()},cols: ${originalMat.cols()}")
-        for (i in 0 until 11){
-            Imgproc.resize(originalMat, scaledOriginalMat, Size(originalMat.cols() * (i * 0.02+1), originalMat.rows() * (i * 0.02+1)))
-            Imgproc.matchTemplate(scaledOriginalMat, templateMat, result, Imgproc.TM_SQDIFF_NORMED)
-            res = Core.minMaxLoc(result)
-            if (currentSimilar == 1.1 || res.minVal < currentSimilar  ){
-                currentSimilar = res.minVal
-                println("i : ${i * 0.02+1}"+"min："+ res.minVal+",max:" +res.maxVal + ", minLoc" + res.minLoc + ", maxLoc" + res.maxLoc)
+    val r = originalMat.rows() / 720
+    picNameList?.forEach picName@{  picName ->
+        val tempMatList = tempMap.getOrPut(picName) {
+            println("getPic:$picPath/$picName.png")
+            val tempMatList = ArrayList<Mat>()
+            val templateMat = getPicture("$picPath/$picName.png")
+            if (r != 1) {
+                Imgproc.resize(
+                    templateMat,
+                    templateMat,
+                    Size(
+                        templateMat.cols() * r.toDouble(),
+                        templateMat.rows() * r.toDouble()
+                    )
+                )
             }
+            for (i in 0 until 5) {
+                Imgproc.resize(
+                    templateMat,
+                    templateMat,
+                    Size(templateMat.cols() * (i * 0.02 + 1), templateMat.rows() * (i * 0.02 + 1))
+                )
+                tempMatList.add(templateMat.clone())
+            }
+            tempMatList
         }
-
-        // 获取最佳匹配位置
-
-
-        // 检查相似度阈值
-        //println("currentSimilar: $currentSimilar, similarityThreshold: $similarityThreshold")
-        if (currentSimilar < similarityThreshold) {
-            if (matchSave) {
-                // 计算模板在原始图像中的位置
-                val templateWidth = templateMat.cols()
-                val templateHeight = templateMat.rows()
-                // 将模板中心点坐标从缩放后的图像映射回原始图像
-                if (is720p) {
+        println("$picName--------------------")
+        tempMatList.forEach {
+            Imgproc.matchTemplate(originalMat, it, result, Imgproc.TM_SQDIFF_NORMED)
+            val res = Core.minMaxLoc(result)
+            println("width : ${it.cols()}"+"min："+ res.minVal+",max:" +res.maxVal + ", minLoc" + res.minLoc + ", maxLoc" + res.maxLoc)
+            if (res.minVal <= similarityThreshold) {
+                if (matchSave) {
+                    // 计算模板在原始图像中的位置
+                    val templateWidth = tempMatList[0].cols() * r
+                    val templateHeight = tempMatList[0].rows() * r
+                    // 将模板中心点坐标从缩放后的图像映射回原始图像
                     sai.point = Point(
                         (res.minLoc.x + templateWidth / 2).toFloat(),
                         (res.minLoc.y + templateHeight / 2).toFloat()
                     )
-                } else {
-                    sai.point = Point(
-                        ((res.minLoc.x + templateWidth / 2) * originalMat.cols() / scaledOriginalWidth).roundToInt()
-                            .toFloat(),
-                        ((res.minLoc.y + templateHeight / 2)* originalMat.rows() / scaledOriginalHeight).roundToInt()
-                            .toFloat()
-                    )
                 }
-            }else{
-                return false
-            }
-        }else{
-            if (matchSave){
-                return false
+                return@picName
             }
         }
     }
-    return true
+    //为空，未找到
+    if (sai.point == null){
+        //若为找到保存，则直接返回false
+        if (matchSave){
+            return false
+        }
+        return true
+    }else{
+        //找到
+        //若为找到保存，则直接返回true继续
+        if (matchSave){
+            return true
+        }
+        //若为【未找到才满足条件】则返回false不继续
+        return false
+    }
 }
 
 fun getPicture(picName : String) : Mat{
