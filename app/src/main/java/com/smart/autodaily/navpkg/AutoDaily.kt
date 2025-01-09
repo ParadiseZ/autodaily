1e1c7cfc35f0ca2f2fefd43913e30b77c0c9f83a
PA <zhuzj2019@qq.com>
2025-01-09 11:22:31 +0800

    feat(AutoDaily): 添加OCR检测功能并重命名检测方法
    • 将`detect`方法重命名为`detectYolo`以明确功能区分
    • 新增`loadOcr`和`detectOcr`方法以支持OCR检测功能
    • 引入`OcrResult`实体类用于存储OCR检测结果

--- app/src/main/java/com/smart/autodaily/navpkg/AutoDaily.kt
+++ app/src/main/java/com/smart/autodaily/navpkg/AutoDaily.kt
@@ -3,13 +3,16 @@ package com.smart.autodaily.navpkg
 import android.content.res.AssetManager
 import android.graphics.Bitmap
 import com.smart.autodaily.data.entity.DetectResult
+import com.smart.autodaily.data.entity.OcrResult
 
 class AutoDaily{
     external fun loadModel(mgr : AssetManager,modelPath : String, targetSize: Int, useGpu : Boolean) : Boolean
     external fun loadModelSec(paramPath : String, modelPath : String, targetSize: Int, useGpu : Boolean) : Boolean
-    external fun detect(capture: Bitmap,numClasses:Int, threshold: Float = 0.25f, nmsThreshold: Float = 0.45f) : Array<DetectResult>
-
+    external fun detectYolo(capture: Bitmap,numClasses:Int, threshold: Float = 0.25f, nmsThreshold: Float = 0.45f) : Array<DetectResult>
     external fun detectAndDraw(capture: Bitmap,numClasses:Int, threshold: Float=0.25f, nmsThreshold: Float=0.5f, drawMap: Bitmap) : Array<DetectResult>
+
+    external fun loadOcr(mgr : AssetManager, lang : Int, useGpu: Boolean) :Boolean
+    external fun detectOcr(capture: Bitmap) : Array<OcrResult>
 
     companion object{
         init {
