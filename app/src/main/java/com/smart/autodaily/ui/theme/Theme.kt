bb5bfdb9a769bbc50a61d85395195e7910d2ccef
ParadiseZ <2461592640@qq.com>
2025-06-05 10:09:34 +0800

    1.1.2 1、在初始获取数据时，新增2秒延迟以避免首次安装时出现的数据库初始化冲突问题 2、新增下载模型文件前，检验文件是否有不同，不同才下载 3、CompareFun新增根据文件计算md5值 4、修复下载进度条不显示的问题 5、修复删除后搜索页下载状态可能不正确的问题 6、修复高安卓版本状态栏颜色异常问题

--- app/src/main/java/com/smart/autodaily/ui/theme/Theme.kt
+++ app/src/main/java/com/smart/autodaily/ui/theme/Theme.kt
@@ -101,7 +101,9 @@ fun AutoDailyTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = if(Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2){
+                !darkTheme
+            }else darkTheme
         }
     }
 
